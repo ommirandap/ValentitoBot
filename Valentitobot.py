9fe8897cefebc73dec4ec8da7da4976dc834b29b
Jazmine <jamaldonadof@ug.uchile.cl>
2016-06-15 20:01:51 -0400

    Le quité el echo y le agregue frases, funciona ctrl+C

--- Valentitobot.py
+++ Valentitobot.py
@@ -1,4 +1,6 @@
-import telegram, logging
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import telegram, logging, random
 from telegram.ext import Updater, CommandHandler
 from telegram.ext import MessageHandler, Filters
 
@@ -11,21 +13,57 @@ print(bot.getMe())
 
 # Mensaje de bienvenida. Cuando inicias una conversacion con ValentitoBot
 def start(bot, update):
-	bot.sendMessage(chat_id=update.message.chat_id, text='Hola, soy tito y amo windows!')
+	bot.sendMessage(chat_id=update.message.chat_id, text='Hola, soy tito y me gustan los gatos :)')
+
 # Handler para el comando /start
 start_handler = CommandHandler('start', start)
 dispatcher = updater.dispatcher
 dispatcher.add_handler(start_handler)
 
 def echo(bot, update):
-        if (update.message.text).find('linux') > 0:
-                bot.sendMessage(chat_id=update.message.chat_id, text='Eso pasa por usar software penca')
-        else:
-                bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
-# Esto fiitra si el usuario envia un mensaje
+	text = update.message.text.lower()
+	user = update.message.from_user
+
+	print user.first_name + ': ' + text
+	
+	if text.find('hola') >= 0:
+		bot.sendMessage(chat_id=update.message.chat_id, text='Hola :)')
+	if text.find('como estas?') >= 0:
+		bot.sendMessage(chat_id=update.message.chat_id, text='Bien y tu?')
+
+	if text.find('linux') >= 0 or text.find('ubuntu') >= 0 or text.find('manjaro') >= 0 or text.find('unix') >= 0:
+		n = random.randint(1,2)
+		if n == 1:
+			bot.sendMessage(chat_id=update.message.chat_id, text='Eso pasa por usar software penca')
+		elif n == 2:
+			bot.sendMessage(chat_id=update.message.chat_id, text='Probaste instalando Windows?')
+	elif text.find('arvoleda') >= 0 or text.find('vegan') >= 0 or text.find('vegeta') >= 0:
+		bot.sendMessage(chat_id=update.message.chat_id, text='Y donde está mi carne?!')
+	elif text.find('lucha') >= 0:
+		bot.sendMessage(chat_id=update.message.chat_id, text='Yo quiero ser Diva')
+	elif user.username.lower() == 'ommirandap':
+		bot.sendMessage(chat_id=update.message.chat_id, text='Ven? por eso el otro Omar era el buen Omar')
+	elif user.username.lower() == 'thebestaround':
+		bot.sendMessage(chat_id=update.message.chat_id, text='Te amo Boris <3')
+	else:
+		n = random.randint(1,30)
+		if n == 10:
+			bot.sendMessage(chat_id=update.message.chat_id, text='Sabías que los gatos anulan las malas vibras del WiFi?')
+		elif n == 15:
+			bot.sendMessage(chat_id=update.message.chat_id, text='Cuando va a estar Egreen para android 4.1.2?')
+		elif n == 5:
+			bot.sendMessage(chat_id=update.message.chat_id, text='Tienes mi sello de aprobación')
+		elif n == 29:
+			bot.sendMessage(chat_id=update.message.chat_id, text='Sabías que los gatos anulan las malas vibras del WiFi?')
+		elif n == 13:
+			bot.sendMessage(chat_id=update.message.chat_id, text='Cuando va a estar Egreen para android 4.1.2?')
+
+# Esto filtra si el usuario envia un mensaje
 echo_handler = MessageHandler([Filters.text], echo)
 dispatcher.add_handler(echo_handler)
 
 # Esto empieza a hacer funcionar el Bot
 updater.start_polling()
 
+# Permite detener el bot usando ctrl+C 
+updater.idle()
--- ideas.md
+++ ideas.md
@@ -1,7 +1,8 @@
 ## IDEAS
 
-- [ ] Si habla @ommirandap, responde "ven? El otro era el buen Omar"
-- [ ] Si se habla de comida veg*, diga "y donde está la carne!?"
-- [ ] Si se habla de Linux/Unix/Manjaro/Ubuntu/etc, diga algo relacionado "Windows es mejor", "Eso te pasa por usar software penca"
-- [ ] Si se habla de lucha, que diga "Yo quiero ser Diva"
+- [X] Si habla @ommirandap, responde "ven? El otro era el buen Omar"
+- [X] Si se habla de comida veg*, diga "y donde está la carne!?"
+- [X] Si se habla de Linux/Unix/Manjaro/Ubuntu/etc, diga algo relacionado "Windows es mejor", "Eso te pasa por usar software penca"
+- [X] Si se habla de lucha, que diga "Yo quiero ser Diva"
 - [ ] Hacer un handler especial que se ponga en los "tito caso de borde"
+- [ ] Frases random como "sabías que los gatos anulan la malas vibras de WiFi?" pensar otras :)
