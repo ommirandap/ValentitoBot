464dea5a80fb47b6ec498e7c035d0c1b1401ec95
Jazmine <jazminemf@gmail.com>
2016-06-16 13:09:02 -0300

    Escribe conversación en un archivo

--- Valentitobot.py
+++ Valentitobot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import telegram, logging, random
+import telegram, logging, random, codecs
 from telegram.ext import Updater, CommandHandler
 from telegram.ext import MessageHandler, Filters
 
@@ -23,40 +23,58 @@ dispatcher.add_handler(start_handler)
 def echo(bot, update):
 	text = update.message.text.lower()
 	user = update.message.from_user
+	respuesta = ''
 
-	print user.first_name + ': ' + text
+	with open('valentitobot.log','a') as f:
+		f.write(user.first_name.encode('utf-8') + ': ' + text.encode('utf-8') + '\n')
 	
 	if text.find('hola') >= 0:
-		bot.sendMessage(chat_id=update.message.chat_id, text='Hola :)')
-	if text.find('como estas') >= 0 or text.find('cómo estás') >= 0 or text.find('cómo estas') >= 0:
-		bot.sendMessage(chat_id=update.message.chat_id, text='Bien y tu?')
+		respuesta = 'Hola :)'
+		bot.sendMessage(chat_id=update.message.chat_id, text=respuesta)
+	if text.find('como estas') >= 0:
+		respuesta = 'Bien y tu?'
+		bot.sendMessage(chat_id=update.message.chat_id, text=respuesta)
 
 	if text.find('linux') >= 0 or text.find('ubuntu') >= 0 or text.find('manjaro') >= 0 or text.find('unix') >= 0:
 		n = random.randint(1,2)
 		if n == 1:
-			bot.sendMessage(chat_id=update.message.chat_id, text='Eso pasa por usar software penca')
+			respuesta = 'Eso pasa por usar software penca'
+			bot.sendMessage(chat_id=update.message.chat_id, text=respuesta)
 		elif n == 2:
-			bot.sendMessage(chat_id=update.message.chat_id, text='Probaste instalando Windows?')
+			respuesta = 'Probaste instalando Windows?'
+			bot.sendMessage(chat_id=update.message.chat_id, text=respuesta)
+	elif text.find('windows') >= 0 or text.find('microsoft') >= 0:
+		respuesta = 'Recuerda que Microsoft me auspicia <3'
+		bot.sendMessage(chat_id=update.message.chat_id, text=respuesta)
 	elif text.find('arvoleda') >= 0 or text.find('vegan') >= 0 or text.find('vegeta') >= 0:
-		bot.sendMessage(chat_id=update.message.chat_id, text='Y donde está mi carne?!')
-	elif user.username.lower() == 'ommirandap' and random.randint(1,16) == 8:
-		bot.sendMessage(chat_id=update.message.chat_id, text='Ven? por eso el otro Omar era el buen Omar')
-	elif user.username.lower() == 'thebestaround' and random.randint(1,16) == 8:
-		bot.sendMessage(chat_id=update.message.chat_id, text='@thebestaround Te amo Boris <3')
+		respuesta = 'Y donde está mi carne?!'
+		bot.sendMessage(chat_id=update.message.chat_id, text=respuesta)
+	elif user.username.lower() == 'ommirandap' and random.randint(1,11) == 4:
+		respuesta = 'Ven? por eso el otro Omar era el buen Omar'
+		bot.sendMessage(chat_id=update.message.chat_id, text=respuesta)
+	elif user.username.lower() == 'thebestaround' and random.randint(1,11) == 6:
+		respuesta = '@thebestaround Te amo Boris <3'
+		bot.sendMessage(chat_id=update.message.chat_id, text=respuesta)
 	elif text.find('lucha') >= 0:
-		bot.sendMessage(chat_id=update.message.chat_id, text='Yo quiero ser Diva')
+		respuesta = 'Yo quiero ser Diva'
+		bot.sendMessage(chat_id=update.message.chat_id, text=respuesta)
 	else:
-		n = random.randint(1,30)
+		n = random.randint(1,15)
 		if n == 10:
-			bot.sendMessage(chat_id=update.message.chat_id, text='Sabías que los gatos anulan las malas vibras del WiFi?')
+			respuesta = 'Sabías que los gatos anulan las malas vibras del WiFi?'
+			bot.sendMessage(chat_id=update.message.chat_id, text=respuesta)
 		elif n == 15:
-			bot.sendMessage(chat_id=update.message.chat_id, text='Cuando va a estar Egreen para android 4.1.2?')
+			respuesta = 'Cuando va a estar Egreen para android 4.1.2?'
+			bot.sendMessage(chat_id=update.message.chat_id, text=respuesta)
 		elif n == 5:
-			bot.sendMessage(chat_id=update.message.chat_id, text='@' + user.nickname + ' tienes mi sello de aprobación')
-		elif n == 29:
-			bot.sendMessage(chat_id=update.message.chat_id, text='Sabías que los gatos anulan las malas vibras del WiFi?')
+			respuesta = 'Tienes mi sello de aprobación'
+			bot.sendMessage(chat_id=update.message.chat_id, text=respuesta)
 		elif n == 13:
-			bot.sendMessage(chat_id=update.message.chat_id, text='Y cuándo va a estar Egreen para android 4.1.2?')
+			respuesta = 'Y cuándo va a estar Egreen para android 4.1.2?'
+			bot.sendMessage(chat_id=update.message.chat_id, text=respuesta)
+
+	with open('valentitobot.log','a') as f:
+                f.write('Respuesta ValentitoBot: ' + respuesta.encode('utf-8') + '\n\n')
 
 # Esto filtra si el usuario envia un mensaje
 echo_handler = MessageHandler([Filters.text], echo)
